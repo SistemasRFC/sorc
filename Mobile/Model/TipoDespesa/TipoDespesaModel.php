c39c71d20f3d45a385a50e584f9f5976368dae18
C1299844 Rafael Freitas Carneiro <c1299844@interno.bb.com.br>
2021-09-24 11:35:54 -0300

    task

--- Mobile/Model/TipoDespesa/TipoDespesaModel.php
+++ Mobile/Model/TipoDespesa/TipoDespesaModel.php
@@ -6,7 +6,7 @@ class TipoDespesaModel extends BaseModel
     Public Function ListarTiposDespesasAtivos($Json=true){
         $dao = new TiposDespesaDao();
         $lista = $dao->ListarTiposDespesasAtivos($_SESSION['cod_cliente_final']);
-        for ($i=0;$i<count($lista);$i++){
+        for ($i=0;$i<count($lista[1]);$i++){
             $lista[1][$i]['ID']=$lista[1][$i]['COD_TIPO_DESPESA'];
             $lista[1][$i]['DSC']=$lista[1][$i]['DSC_TIPO_DESPESA'];
         }
