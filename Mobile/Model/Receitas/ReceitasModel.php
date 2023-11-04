6b8d860400d717c379e2e4275ea65eee1c206a5d
Fernanda Lopes <fernandalassis21@gmail.com>
2023-11-04 19:49:15 -0300

    correções Receita

--- Mobile/Model/Receitas/ReceitasModel.php
+++ Mobile/Model/Receitas/ReceitasModel.php
@@ -1,9 +1,8 @@
 <?php
-include_once("Model/BaseModel.php");
 include_once("Dao/Receitas/ReceitasDao.php");
-include_once("Resources/php/FuncoesArray.php");
-include_once("Resources/php/FuncoesData.php");
-include_once("Resources/php/FuncoesMoeda.php");
+include_once("../Resources/php/FuncoesData.php");
+include_once("../Resources/php/FuncoesMoeda.php");
+include_once("../Resources/php/FuncoesArray.php");
 class ReceitasModel extends BaseModel {
 
     function AddReceitas(){
@@ -63,42 +62,6 @@ class ReceitasModel extends BaseModel {
             $codigoRef = $arrCodigos[$i];
             $lista = $dao->ImportarReceita($_SESSION['cod_cliente_final'], $dtaReceita, $codigoRef);
         }
-
-        // $mes = filter_input(INPUT_POST, 'dtaReceita', FILTER_SANITIZE_STRING);  
-        // $codigo = filter_input(INPUT_POST, 'codReceita', FILTER_SANITIZE_STRING); 
-        // $mes = explode('/', $mes);
-        // $mesAtual = date("m");
-        // $anoAtual = date("Y");
-        // if ($mes[1]==$mesAtual){
-        //     $lista[0]=false;
-        //     $lista[1]="Esta receita pertence a este mês já!";
-        // }else{
-        //     if ($mes[1]==12){
-        //         $mes[1]='01';
-        //         $mes[2]=$mes[2]+1;
-        //     }else{
-        //         $mes[1]=$mes[1]+1;
-        //     }
-        //     if ($mes[1]<10){
-        //         $mes[1]='0'.$mes[1];
-        //     }
-        //     $data = $mes[0].'/'.$mesAtual.'/'.$anoAtual;
-        //     $dao = new ReceitasDao();
-        //     $ret = $dao->VerificaReceita($codigo);
-        //     if ($ret[0]){
-        //         if ($ret[1][0]['QTD']>0){
-        //             $lista[0]=false;
-        //             $lista[1]='Esta receita já foi importada para este mês';
-        //         }else{
-        //             $lista = $dao->ImportarReceita($_SESSION['cod_cliente_final'],
-        //                                       $data,
-        //                                       $codigo);
-        //         }
-        //     }else{
-        //         $lista=$ret;
-        //     }
-            
-        // }
         return json_encode($lista);
     }
     
